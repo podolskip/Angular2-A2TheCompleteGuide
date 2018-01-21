a1b7b95f631e05b844b64923d4ffc9c08f13cb52
Patryk Podolski <podolski.patryk@gmail.com>
2018-01-21 18:20:20 +0100

    first commit start with video 014

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'first-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'testing configuration of first angular 2 app';
 }
