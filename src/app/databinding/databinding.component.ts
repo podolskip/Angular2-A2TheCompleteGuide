09977814180891c06a96443d24f1aaaaa0bad3c5
Patryk Podolski <podolski.patryk@gmail.com>
2018-01-27 17:43:44 +0100

    property binging added

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { PropertyBindingComponent } from './property-binding.component';
 
 @Component({
   selector: 'app-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  styleUrls: ['./databinding.component.css'],
+  directives: [PropertyBindingComponent]
 })
 export class DatabindingComponent  {
   stringInterpolation = 'this is string interpolation!';
   numberInterpolation = 2;
 
+
+
+  onTest(){
+    return true;
+}
 }
--- src/app/databinding/index.ts
+++ src/app/databinding/index.ts
@@ -2,3 +2,5 @@
  * Created by pat on 2018-01-27.
  */
 export { DatabindingComponent } from './databinding.component';
+export { PropertyBindingComponent } from './property-binding.component';
+
